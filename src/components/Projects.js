536cde1487e9b3a1a9de572483600982444e6582
valdezDev <pvaldezdev@gmail.com>
2020-11-09 20:09:08 -0500

    typo fix in Project mapping

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,9 @@ const Projects = ({ projects, title, showLink }) => {
     <section className="section projects">
       <Title title={title} />
       <div className="section-center projects-center">
-        {projects.map((project, index) => {return <Project key={project.id} index={index} {...project}})}
+        {projects.map((project, index) => {
+          return <Project key={project.id} index={index} {...project} />
+        })}
       </div>
       {showLink && (
         <Link to="/projects" className="btn center-btn">
