93d5b4aebb4a7f46e16a1fab283182776f4c5a93
valdezDev <pvaldezdev@gmail.com>
2020-11-10 14:29:36 -0500

    changed Blogs component title in the index page, created blog pages programatically using a unique query that is accessing generated slugs in node

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default ({ data }) => {
       <Services />
       <Jobs />
       <Projects projects={projects} title="featured projects" showLink />
-      <Blogs blogs={blogs} title="blog" showLink />
+      <Blogs blogs={blogs} title="latest blogs" showLink />
     </Layout>
   )
 }
