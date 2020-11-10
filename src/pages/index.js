8ce6441f4c45f318f723be30ceba43ecc3670848
valdezDev <pvaldezdev@gmail.com>
2020-11-10 12:59:02 -0500

    imported allStrapiBlogs query and rendered the Blog component into Blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import Blogs from "../components/Blogs"
 export default ({ data }) => {
   const {
     allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs },
   } = data
 
   return (
@@ -18,6 +19,7 @@ export default ({ data }) => {
       <Services />
       <Jobs />
       <Projects projects={projects} title="featured projects" showLink />
+      <Blogs blogs={blogs} title="blog" showLink />
     </Layout>
   )
 }
@@ -41,6 +43,31 @@ export const query = graphql`
         stack {
           id
           title
+        }
+      }
+    }
+
+    allStrapiBlogs(sort: { fields: date, order: DESC }, limit: 10) {
+      nodes {
+        slug
+        content
+        date(formatString: "MMMM Do, YYYY")
+        desc
+        id
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+    allSiteBuildMetadata {
+      edges {
+        node {
+          id
         }
       }
     }
