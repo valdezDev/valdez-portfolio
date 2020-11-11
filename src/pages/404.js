3bb8f3364bb3a64326ea9574bbe4709d4202f6bc
valdezDev <pvaldezdev@gmail.com>
2020-11-11 09:49:52 -0500

    created a template for my error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,20 @@ import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <main className="error-page">
+        <div className="error-container">
+          <h1>sorry about that! </h1>
+          <h2>Looks like something went wrong...</h2>
+          <h4>You can directly email me at pvaldezdev@gmail.com</h4>
+          <Link to="/" className="btn">
+            back home
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
