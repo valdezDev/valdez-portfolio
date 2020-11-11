9611988daaea095ef05ab546562e446d8396e532
valdezDev <pvaldezdev@gmail.com>
2020-11-11 11:47:27 -0500

    pointing gasby config at strapi backend hosted on heroku

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `https://git.heroku.com/valdez-strapi-portfolio.git`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`jobs`, `projects`, `blogs`],
         singleTypes: [`about`],
